fbb619c55d6352329af14f55233106c12c4dfd12
Ahamedhsenid <ahamed.a@hsenidlanka.com>
2024-07-26 14:27:47 +0530

    Added initial setup files

---
+++ src/test/java/com/hsenid/Paypro/PayproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsenid.Paypro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
